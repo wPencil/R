7647caeee1fcd4360d0596528773f1989b43d879
Wstayyoung <438304835@qq.com>
2023-09-13 20:15:56 +0800

    line chart

--- line chart.R
+++ line chart.R
@@ -7,6 +7,7 @@
 # 2.Time series data
 #     2.1 one variable
 #     2.2 two variable
+# function: line_chart()--------------------------------------------------------
 
 # 0.packages--------------------------------------------------------------------
 library(ggplot2)
@@ -27,8 +28,8 @@ ggplot(data = test, mapping = aes(x=Time, y=weight))+
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_continuous(breaks = unique(test$Time))
 # 1.2 two group-----------------------------------------------------------------
-test1 <- ChickWeight[1:24,] # Chick1, Diet 1 and 2
-ggplot(data = test1, mapping = aes(x=Time, y=weight, color=Chick))+
+test <- ChickWeight[1:24,] # Chick1, Diet 1 and 2
+ggplot(data = test, mapping = aes(x=Time, y=weight, color=Chick))+
   geom_line()+
   geom_point()+
   theme_classic()+ # theme_gray() by default
@@ -38,7 +39,7 @@ ggplot(data = test1, mapping = aes(x=Time, y=weight, color=Chick))+
        #x = "XXXXX", y = "XXXXX"
        )+
   theme(plot.title = element_text(hjust = 0.5)) +
-  scale_x_continuous(breaks = unique(test1$Time))
+  scale_x_continuous(breaks = unique(test$Time)) # 
 
 # 2.Time series data------------------------------------------------------------
 # 2.1 one variable--------------------------------------------------------------
@@ -46,8 +47,8 @@ df <- data.frame(date = seq(as.Date("2021-01-01"), as.Date("2021-12-31"), by = "
                  value = rnorm(12))
 df$date <- as.Date(df$date)
 
-test2 <- df
-ggplot(data = test2, mapping = aes(x=date, y=value))+
+test <- df
+ggplot(data = test, mapping = aes(x=date, y=value))+
   geom_line()+
   geom_point()+
   theme_classic()+ # theme_gray() by default
@@ -57,14 +58,25 @@ ggplot(data = test2, mapping = aes(x=date, y=value))+
   )+
   theme(plot.title = element_text(hjust = 0.5))
 
-# 2.2 two variable--------------------------------------------------------------
-df1 <- data.frame(date = seq(as.Date("2021-01-01"), as.Date("2021-12-31"), by = "month"),
-                 value = rnorm(12))
-df1$date <- as.Date(df1$date)
+rm(list = ls())
 
-test3 <- rbind(df, df1)
-test3$vari <- c(rep(1,12), rep(2,12)) %>% as.factor()
-ggplot(data = test3, mapping = aes(x=date, y=value, color=vari))+
+# 2.2 two variable--------------------------------------------------------------
+set.seed(1)
+value1 <- rnorm(12)
+set.seed(2)
+value2 <- rnorm(12)
+df <- data.frame(date = seq(as.Date("2021-01-01"), as.Date("2021-12-31"), by = "month"),
+                 value1,
+                 value2)
+df$date <- as.Date(df$date)
+
+# wide-format -> long-format
+library(reshape2)
+df_l <- melt(df, id.vars = "date")
+
+test <- df_l
+
+ggplot(data = test, mapping = aes(x=date, y=value, color=variable))+
   geom_line()+
   geom_point()+
   theme_classic()+ # theme_gray() by default
@@ -74,4 +86,24 @@ ggplot(data = test3, mapping = aes(x=date, y=value, color=vari))+
        #x = "XXXXX", y = "XXXXX"
   )+
   theme(plot.title = element_text(hjust = 0.5))+
-  scale_x_continuous(breaks = unique(test3$date)) # show all
+  scale_x_continuous(breaks = unique(test$date)) # show all
+
+# function----------------------------------------------------------------------
+line_chart <- function(test, x0, y0, title0=NULL,groups0=NULL, colors0=NULL,
+                       x_labs0=NULL, y_labs0=NULL, legend_title=NULL){
+  library(ggplot2)
+  
+  ggplot(data = test, mapping = aes(x=x0, y=y0, color=groups0))+
+    geom_line()+
+    geom_point()+
+    theme_classic()+ # theme_gray() by default
+    scale_color_manual(values = colors0)+ # set group color
+    labs(title = title0, 
+         x = x_labs0, 
+         y = y_labs0,
+         colour = legend_title,
+         )+
+    theme(plot.title = element_text(hjust = 0.5))+
+    scale_x_continuous(breaks = unique(x0)) # show all
+}
+line_chart(test, test$date, test$value, groups0=test$variable, colors0=c("red","blue"))
