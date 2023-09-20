25bfd43085d23cee5d255752555e459706290963
Wstayyoung <438304835@qq.com>
2023-09-20 14:19:02 +0800

    Survival analysis

--- Survival analysis.R
+++ Survival analysis.R
@@ -10,25 +10,34 @@ library(survminer)
 # Sample data
 head(lung)
 
+table(lung$sex)
 fit <- survfit(Surv(time, status) ~ sex, data = lung) 
 
 ggsurvplot(fit)
 ggsave("Survival analysis.png", dpi = 500)
 
 # 2. my-------------------------------------------------------------------------
-test0 <- fit
-time0 <- lung$time
-ggsurvplot(test0,
-           pval = TRUE, 
-           pval.method = TRUE, pval.method.coord=c(0.77*max(time0),1),
-           pval.title = "right",pval.coord = c(0.75*max(time0), 0.9), 
-           risk.table = TRUE, 
-           risk.table.col = "strata", 
+cox <- coxph(Surv(time, status) ~ sex, data = lung)
+res <- summary(cox)
+x <- lung$time
+
+p<-ggsurvplot(fit,
+           pval = TRUE, pval.coord = c(0.8*max(x), 1),
+           pval.method = TRUE, pval.method.coord=c(0.65*max(x),1),
+           risk.table = TRUE, risk.table.col = "strata",
            ggtheme = theme_classic(), 
            palette = c("darkblue", "red"), 
-           legend.title = "legend title", 
-           legend.labs = c("labs1", "labs2"),
-           
-           # xlim = c(0,10), break.x.by = 2, xlab = "Years", 
-           # ylab = "Relapse-free survival probability" 
-)
+           legend.title = "Gender",
+           legend.labs = c("Male", "Female"),
+           xlab = "Time(days)", xlim = c(0,1000), break.x.by = 200, 
+           ylab = "Survival probability"
+           )
+p$plot <- p$plot+
+  ggtitle("My Survival Plot")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  annotate("text",x=0.8*max(x),y=0.92, label=paste0("HR(95%CI) = ", round(res$conf.int[1],2), "(",round(res$conf.int[3],2),"-", round(res$conf.int[4],2), ")" ))+
+  annotate("text",x=0.8*max(x),y=0.84, label=paste0("C-index = ", round(res$concordance[1],2)))
+
+png("My Survival analysis.png", width = 7500, height = 6000, res = 1000)
+p
+dev.off()
