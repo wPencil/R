f0cbbad140e86d0283acc24c96482e40d7e9695b
Wstayyoung <438304835@qq.com>
2023-09-28 22:30:54 +0800

    MyPackage

--- MyPackage/R/fig_survival.R
+++ MyPackage/R/fig_survival.R
@@ -1,29 +1,29 @@
-loop_survival_analysis <- function(object0, file_name, ylab0, xlab0, break_x0,
-                                   xlim0, legend_title0, legend_labs0){
-  library(survival)
-  library(survminer)
-  
+fig_survival <- function(object0, file_name, ylab0, xlab0, break_x0,
+                         xlim0, legend_title0, legend_labs0){
+  # library(survival)
+  # library(survminer)
+
   # survfit()-------------------------------------------------------------------
-  fit <- surv_fit(Surv(time, status) ~ groups, data = object0) 
-  
+  fit <- surv_fit(Surv(time, status) ~ groups, data = object0)
+
   # cox-------------------------------------------------------------------------
   cox <- coxph(Surv(time, status) ~ groups, data = object0)
   res <- summary(cox)
   x <- object0$time
-  
+
   # fig-------------------------------------------------------------------------
   p<-ggsurvplot(fit,
                 pval = TRUE, pval.coord = c(0.7*max(x), 1),
                 pval.method = TRUE, pval.method.coord=c(0.4*max(x),1),
                 risk.table = TRUE, risk.table.col = "strata",
-                ggtheme = theme_linedraw(), 
-                palette = c("darkblue", "red"), 
+                ggtheme = theme_linedraw(),
+                palette = c("darkblue", "red"),
                 legend.title = legend_title0,
                 legend.labs = legend_labs0,
-                xlab = xlab0, xlim = xlim0, break.x.by = break_x0, 
+                xlab = xlab0, xlim = xlim0, break.x.by = break_x0,
                 ylab = ylab0
   )
-  
+
   p$plot <- p$plot+
     theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
     # ggtitle("My Survival Plot")+
@@ -32,7 +32,7 @@ loop_survival_analysis <- function(object0, file_name, ylab0, xlab0, break_x0,
     annotate("text",x=0.7*max(x),y=0.83, label=paste0("C-index = ", round(res$concordance[1],2)))
   p$table <- p$table+
     theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
-  
+
   tiff(file_name, width = 4290, height = 4930, res = 1000)
   print(p)
   dev.off()
