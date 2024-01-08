d44b7ce5d0e0622684a5c662da96105f1ca196d3
Wstayyoung <438304835@qq.com>
2024-01-08 09:59:46 +0800

    confusion_matrix

--- MyPackage/R/fig_confusion_matrix.R
+++ MyPackage/R/fig_confusion_matrix.R
@@ -1,16 +1,16 @@
-draw_confusion_matrix <- function(cm, label1, label2) {
+fig_confusion_matrix <- function(cm, label1, label2) {
   # Confusion matrix diagram
   #       label1  label2
   # label1
   # label2
   # read: Labels need to be modified
-  
+
   layout(matrix(c(1,1,2)))
   par(mar=c(2,2,2,2))
   plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n',
        yaxt='n')
   title('CONFUSION MATRIX', cex.main=2)
-  
+
   # create the matrix
   rect(150, 430, 240, 370, col='#3F97D0')
   text(195, 435, label1, cex=1.2) # label1--------------------------------------
@@ -22,14 +22,14 @@ draw_confusion_matrix <- function(cm, label1, label2) {
   rect(250, 305, 340, 365, col='#3F97D0')
   text(140, 400, label1, cex=1.2, srt=90) # label1------------------------------
   text(140, 335, label2, cex=1.2, srt=90) # label2------------------------------
-  
+
   # add in the cm results
   res <- as.numeric(cm$table)
   text(195, 400, res[1], cex=1.6, font=2, col='white')
   text(195, 335, res[2], cex=1.6, font=2, col='white')
   text(295, 400, res[3], cex=1.6, font=2, col='white')
   text(295, 335, res[4], cex=1.6, font=2, col='white')
-  
+
   # add in the specifics
   plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETA
 ILS", xaxt='n', yaxt='n')
@@ -43,10 +43,10 @@ ILS", xaxt='n', yaxt='n')
   text(70, 70, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
   text(90, 85, names(cm$byClass[7]), cex=1.2, font=2)
   text(90, 70, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
-  
+
   # add in the accuracy information
   text(30, 35, names(cm$overall[1]), cex=1.5, font=2)
   text(30, 20, round(as.numeric(cm$overall[1]), 3), cex=1.4)
   text(70, 35, names(cm$overall[2]), cex=1.5, font=2)
   text(70, 20, round(as.numeric(cm$overall[2]), 3), cex=1.4)
-} 
+}
