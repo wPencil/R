d0e50933cf13734aa92813fdf23d0d1072f49bb4
wyh <438304835@qq.com>
2024-01-10 12:56:32 +0800

    Draw survival curve

--- MyPackage/R/fig_survival.R
+++ MyPackage/R/fig_survival.R
@@ -1,5 +1,5 @@
 fig_survival <- function(object, file_name, ylab0, xlab0, break_x0,
-                         xlim0, legend_title0, legend_labs0){
+                         xlim0, legend_title0, legend_labs0, legend_pos="top right corner"){
   # readme----------------------------------------------------------------------
   # The format of input object. data.frame()
   #       1     2       3
@@ -20,7 +20,8 @@ fig_survival <- function(object, file_name, ylab0, xlab0, break_x0,
   x <- object$time
 
   # fig-------------------------------------------------------------------------
-  p<-ggsurvplot(fit,
+  if(legeng_pos == "top right corner"){
+    p<-ggsurvplot(fit,
                 pval = TRUE, pval.coord = c(0.7*max(x), 1),
                 pval.method = TRUE, pval.method.coord=c(0.4*max(x),1),
                 risk.table = TRUE, risk.table.col = "strata",
@@ -32,13 +33,38 @@ fig_survival <- function(object, file_name, ylab0, xlab0, break_x0,
                 ylab = ylab0
                 )
 
-  p$plot <- p$plot+
-    theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
-    theme(plot.title = element_text(hjust = 0.5))+
-    annotate("text",x=0.7*max(x),y=0.91, label=paste0("HR(95%CI) = ", round(res$conf.int[1],2), "(",round(res$conf.int[3],2),"-", round(res$conf.int[4],2), ")" ))+ # add HR(95%CI)
-    annotate("text",x=0.7*max(x),y=0.83, label=paste0("C-index = ", round(res$concordance[1],2))) # add C-index
-  p$table <- p$table+
-    theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+    p$plot <- p$plot+
+      theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+      theme(plot.title = element_text(hjust = 0.5))+
+      annotate("text",x=0.7*max(x),y=0.91, label=paste0("HR(95%CI) = ", round(res$conf.int[1],2), "(",round(res$conf.int[3],2),"-", round(res$conf.int[4],2), ")" ))+ # add HR(95%CI)
+      annotate("text",x=0.7*max(x),y=0.83, label=paste0("C-index = ", round(res$concordance[1],2))) # add C-index
+
+    p$table <- p$table+
+      theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+  }
+
+  if(legeng_pos == "lower left corner"){
+    p<-ggsurvplot(fit,
+                  pval = TRUE, pval.coord = c(0.32*max(x), 0.18),
+                  pval.method = TRUE, pval.method.coord=c(0.03*max(x),0.18),
+                  risk.table = TRUE, risk.table.col = "strata",
+                  ggtheme = theme_linedraw(),
+                  palette = c("darkblue", "red"),
+                  legend.title = legend_title0,
+                  legend.labs = legend_labs0,
+                  xlab = xlab0, xlim = xlim0, break.x.by = break_x0,
+                  ylab = ylab0)
+
+    p$plot <- p$plot+
+      theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+      theme(plot.title = element_text(hjust = 0.5))+
+      annotate("text",x=0.3*max(x),y=0.09, label=paste0("HR(95%CI) = ", round(res$conf.int[1],2), "(",round(res$conf.int[3],2),"-", round(res$conf.int[4],2), ")" ))+ # add HR(95%CI)
+      annotate("text",x=0.3*max(x),y=0.01, label=paste0("C-index = ", round(res$concordance[1],2))) # add C-index
+
+    p$table <- p$table+
+      theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+  }
+
 
   # Save as tiff image----------------------------------------------------------
   tiff(file_name, width = 4290, height = 4930, res = 1000)
