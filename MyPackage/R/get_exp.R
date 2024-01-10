fed1111dab21b19805a1bec691b7fdbe8aaaad5a
wyh <438304835@qq.com>
2024-01-10 12:22:43 +0800

    convert the probe id to the gene id

--- MyPackage/R/get_exp.R
+++ MyPackage/R/get_exp.R
@@ -1,23 +1,21 @@
 get_exp <- function(GSE_name){
   # get the raw expression matrix from the "GSE_series_matrix.txt"
-  
+
   library(data.table)
   library(magrittr)
-  
+
   # import the series matrix
   GSE <- fread(GSE_name, fill = TRUE)
-  
-  row_exp_mat_begin <- which(GSE == "!series_matrix_table_begin", arr.ind = T
-                       RUE)[, 1] + 1
-  row_exp_mat_end <- which(GSE == "!series_matrix_table_end", arr.ind = TRUE)
-  [, 1] - 1
-  
+
+  row_exp_mat_begin <- which(GSE == "!series_matrix_table_begin", arr.ind = TRUE)[, 1] + 1
+  row_exp_mat_end <- which(GSE == "!series_matrix_table_end", arr.ind = TRUE)[, 1] - 1
+
   exp_df <- GSE[c(row_exp_mat_begin:row_exp_mat_end),]
   samples_id <- exp_df[1, -1] %>% unlist()
   probe_id <- exp_df[-1, 1] %>% unlist()
   exp_mat <- exp_df[-1, -1] %>% as.matrix()
   exp_mat <- apply(exp_mat, c(1,2), as.numeric) # convert to numeric value type
-  
+
   exp_raw <- data.frame(probe_id, exp_mat) # the next step is preprocessing
   names(exp_raw) <- c("probe_id", samples_id)
   return(exp_raw)
