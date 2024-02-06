62522aead7173f432c00d1e75d0b4daedbd97b10
wPencil <109512465+wPencil@users.noreply.github.com>
2024-02-06 08:43:59 +0800

    get_exp.R
    Get the expression matrix from a gz archive

--- MyPackage/R/get_exp.R
+++ MyPackage/R/get_exp.R
@@ -1,24 +1,14 @@
-get_exp <- function(GSE_name){
-  # get the raw expression matrix from the "GSE_series_matrix.txt"
+get_exp <- function(GSE_name) {
 
-  library(data.table)
-  library(magrittr)
+  # get the raw expression matrix from the "GSE_series_matrix.txt.gz"
   library(R.utils)
 
-  # import the series matrix
-  GSE_txt <- gunzip("GSE46862_series_matrix.txt.gz", remove = FALSE)
-  GSE <- fread(GSE_txt, fill = TRUE)
+  GSE_txt <- gunzip(GSE_name, remove = FALSE)
+  GSE_exp <- read.table(GSE_txt, header = TRUE, comment.char = "!")
+  # comment.char = "!": "!" followed by the contents of the comment, do not read
 
-  row_exp_mat_begin <- which(GSE == "!series_matrix_table_begin", arr.ind = TRUE)[, 1] + 1
-  row_exp_mat_end <- which(GSE == "!series_matrix_table_end", arr.ind = TRUE)[, 1] - 1
+  rownames(GSE_exp) <- GSE_exp[, 1]
+  GSE_exp <- GSE_exp[, -1]
 
-  exp_df <- GSE[c(row_exp_mat_begin:row_exp_mat_end),]
-  samples_id <- exp_df[1, -1] %>% unlist()
-  probe_id <- exp_df[-1, 1] %>% unlist()
-  exp_mat <- exp_df[-1, -1] %>% as.matrix()
-  exp_mat <- apply(exp_mat, c(1,2), as.numeric) # convert to numeric value type
-
-  exp_raw <- data.frame(probe_id, exp_mat) # the next step is preprocessing
-  names(exp_raw) <- c("probe_id", samples_id)
-  return(exp_raw)
+  return(GSE_exp)
 }
