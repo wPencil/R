f156c9a4cf472817d7afe9577e7314d41d2c96db
Wstayyoung <438304835@qq.com>
2024-04-19 13:37:30 +0800

    optimal group

--- MyPackage/R/get_surv_optimal_cut.R
+++ MyPackage/R/get_surv_optimal_cut.R
@@ -1,13 +1,15 @@
 get_surv_optimal_cut <- function(object, time0, event0, vari){
-  
-  res.cut <- surv_cutpoint(object, 
-                           time = time0, 
+
+  res.cut <- surv_cutpoint(object,
+                           time = time0,
                            event = event0,
                            variables = vari
                            )
-  
+
   res.cat <- surv_categorize(res.cut)
-  
+
+  res.cat[,3] <- factor(res,cat[,3], levels = c(1,2), labels = c("low", "high"))
+
   return(res.cat)
 
   }
